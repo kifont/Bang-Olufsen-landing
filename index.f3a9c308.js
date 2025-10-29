89279155729b599eb4ff54a32b0ac576f5e75462
Anton Kryzhanivskyi <firechant2@gmail.com>
2025-10-29 15:43:11 +0200

    make build

---
+++ index.f3a9c308.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("contactForm").addEventListener("submit",function(t){t.preventDefault(),window.alert("Форма отправлена!"),this.reset()})});
+//# sourceMappingURL=index.f3a9c308.js.map
